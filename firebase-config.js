94995bb34036d51fe3d0bae7160770cb6b8133cc
crisfina <cristinap.azu@gmail.com>
2026-01-21 11:49:17 +0100

    Refactor Firebase initialization to check existing apps

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjOTcCTg2GCNwlGgsXFRqkP0UGlc8U2Gw",
-  authDomain: "los-chotos.firebaseapp.com",
-  projectId: "los-chotos",
-  storageBucket: "los-chotos.firebasestorage.app",
-  messagingSenderId: "246176234302",
-  appId: "1:246176234302:web:35efecf153e7b3e53c0a6f"
+    apiKey: "AIzaSyCjOTcCTg2GCNwlGgsXFRqkP0UGlc8U2Gw",
+    authDomain: "los-chotos.firebaseapp.com",
+    projectId: "los-chotos",
+    storageBucket: "los-chotos.firebasestorage.app",
+    messagingSenderId: "246176234302",
+    appId: "1:246176234302:web:35efecf153e7b3e53c0a6f"
 };
 
-const app = initializeApp(firebaseConfig);
+// Usamos una variable simple sin 'const' para que no de error al re-declarar
+let app;
+
+if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+} else {
+    app = getApps()[0];
+}
+
 export const db = getFirestore(app);
